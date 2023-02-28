2d7186abff0a8f8dc1547253735f5aedfb9a0c35
Andrei Solntsev <andrei.solntsev@gmail.com>
2023-03-01 00:00:29 +0200

    fix compilation error after upgrading Selenium

--- test/demo/Selenium.java
+++ test/demo/Selenium.java
@@ -1,13 +1,13 @@
 package demo;
 
-import static com.codeborne.selenide.Selenide.close;
+import static com.codeborne.selenide.Selenide.closeWebDriver;
 import static com.codeborne.selenide.Selenide.open;
 
 public class Selenium {
   public static void main(String[] args) {
     for (int i = 0; i < 5; i++) {
       open("http://google.com");
-      close();
+      closeWebDriver();
     }
   }
 }
